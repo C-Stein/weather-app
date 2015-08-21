fe8464d7b133ad192600a4fdaeb6fd445c7016bf
Caitlin Stein <caitlin.teter@gmail.com>
2015-08-21 14:43:30 -0500

    longer forecast functions complete

--- javascripts/core-logic.js
+++ javascripts/core-logic.js
@@ -5,14 +5,25 @@ define(function(require) {
   var firebase = require("firebase");
   var auth = require("authentication");
   var display = require("display-weather");
+  var longer = require("longerForecast");
 
     
   $("#search").on("click", function(){
     var zipCode = $("#searchText").val();
 
     console.log("zipCode", zipCode);
-    display.addWeather(zipCode);
+    weather = display.addWeather(zipCode);
   });
+
+   $(document).on("click", '.threeDay', function() {
+
+      weather3 = longer.get3Day(weather.id);
+   });
+
+   $(document).on("click", '.sevenDay', function() {
+
+      weather7 = longer.get7Day(weather.id);
+   });
 
 
 
--- javascripts/longerForecast.js
+++ javascripts/longerForecast.js
@@ -2,10 +2,34 @@ define(function(require) {
   
 
   return {
-    get3day: function(zip) {
+    get3day: function(ID) {
       $.ajax({
-      url:"???"
+      url:"http://api.openweathermap.org/data/2.5/forecast/daily?id=" + ID + "&units=imperial&cnt=3"
       }).done(function(data){
+
+        var weather3 = {
+          "day1": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day2": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity":data
+          },
+          "day3": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          }
+
+        };
+
+        return weather3;
         
       })
       .fail(function (xhr, status, error){
@@ -14,9 +38,56 @@ define(function(require) {
     },
     get7day: function(zip) {
       $.ajax({
-      url:"???"
+      url:"http://api.openweathermap.org/data/2.5/forecast/daily?id=" + ID + "&units=imperial&cnt=7"
       }).done(function(data){
         
+        var weather7 = {
+          "day1": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day2": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity":data
+          },
+          "day3": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day4": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day5": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day6": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          },
+          "day7": {
+            "temp": data,
+            "pressure": data,
+            "wind-speed": data,
+            "humidity": data
+          }
+
+        };
+
+        return weather7;        
       })
       .fail(function (xhr, status, error){
         
