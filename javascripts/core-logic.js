b568aa961644eb7f0389b42225120a3d1f744580
Jeremy <jeremygrondahl@gmail.com>
2015-08-22 16:19:20 -0500

    Created HBS files to display 3 day and 7 day weather conditions. Made changes in the JS files to grab data from the weather API.

--- javascripts/core-logic.js
+++ javascripts/core-logic.js
@@ -11,11 +11,11 @@ define(function(require) {
 
 
     $("#search").on("click", function() {
-        var zipCode = $("#searchText").val();
-        weather = display.addWeather(zipCode);
-        console.log("zipCode", zipCode);
+    var zipCode = $("#searchText").val();
+    var weather = display.addWeather(zipCode);
 
-        console.log(weather);
+        console.log("zipCode", zipCode);
+        console.log("weather", weather);
 
 
     });
@@ -23,13 +23,24 @@ define(function(require) {
 
 
     $(document).on("click", '.threeDay', function() {
+      zipCode = $("#searchText").val();
+      weather = display.addWeather(zipCode);
 
-        weather3 = longer.get3Day(weather.id);
+      weather3 = longer.get3day(weather.id);
+
+      console.log("weather3", weather3);
+      console.log("weather.id", weather.id);
     });
 
     $(document).on("click", '.sevenDay', function() {
+      var zipCode = $("#searchText").val();
+      var weather = display.addWeather(zipCode);
 
-        weather7 = longer.get7Day(weather.id);
+      weather7 = longer.get7day(weather.id);
+
+      console.log("weather7", weather7);
+      console.log("weather.id", weather.id);
+
     });
 
 
