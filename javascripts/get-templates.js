3fd8703d23dd335490e72e45efbefcfbda7dc7e7
Jeremy <jeremygrondahl@gmail.com>
2015-08-23 02:05:50 -0500

    recovered missing code

--- javascripts/get-templates.js
+++ javascripts/get-templates.js
@@ -1,5 +1,7 @@
 define(function(require) {
 	return {
-		weatherTpl: require("hbs!../templates/weather")
+		weatherTpl: require("hbs!../templates/weather"),
+    weather3Tpl: require("hbs!../templates/weather_3day"),
+    weather7Tpl: require("hbs!../templates/weather_7day")
 	};
 });
